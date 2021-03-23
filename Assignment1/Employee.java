0f2c796eba9d39089c3029fb84886c2839ddeaa5
burakent <73474327+burakent@users.noreply.github.com>
2021-03-23 14:31:47 +0300

    Add files via upload

---
+++ Assignment1/Employee.java
@@ -0,0 +1,18 @@
+public abstract class Employee extends Person {
+
+	// Do not change anything in this class
+
+	protected int yearOfSeniority;
+
+
+	public Employee() {
+
+	}
+	public Employee(String name, int yearOfSeniority) {
+
+		this.name = name;
+		this.yearOfSeniority = yearOfSeniority;
+		number++;
+	}
+
+}
---
+++ Assignment1/SoftwareDevelopment.java
@@ -0,0 +1,13 @@
+public class SoftwareDevelopment extends Department {
+
+
+    public SoftwareDevelopment(String name, int numberOfWorker) {
+        super(name,numberOfWorker);
+
+    }
+    // Erase the content. Fill in the blanks. It returns number.
+    public static int getNumber() {
+        return number;
+    }
+
+}
